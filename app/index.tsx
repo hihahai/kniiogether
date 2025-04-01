9168a99b77392e62ff0f4d319d89101572c95148
vvv3894 <gkgk3894@gmail.com>
2025-04-02 00:39:03 +0900

    1

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>기초 캡스톤 디자인 .</Text>
+    </View>
+  );
+}
