c08915b079555fd1e104676c6496fefbafbea788
haim <gkgk3894@gmail.com>
2025-05-12 15:10:12 +0900

    0512 mac

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -18,7 +18,7 @@ export default function TabLayout() {
   //         }}
   //         />
   //    ) 
-  //  }}
+  //  }
    />
         <Tabs.Screen name='Explore'/>
         <Tabs.Screen name='Cookbook'/>
